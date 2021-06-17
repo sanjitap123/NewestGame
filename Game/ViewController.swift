5aeb3285c91c9d8bb99500954e9cb0e842adc52f
sanjita <sanjita@icloud.com>
2021-06-17 10:07:59 -0700

    change

--- Game/ViewController.swift
+++ Game/ViewController.swift
@@ -17,8 +17,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
      super.viewDidLoad()
      configureSongs()
-        table.delegate = self
-        table.dataSource = self
+        table?.delegate = self
+        table?.dataSource = self
    
     }
     
@@ -28,7 +28,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                           albumName: "Sour",
                           artistName: "Olivia Rodrigo",
                           imageName: "cover1",
-                          trackName: "s"))
+                          trackName: "brutal.mp3"))
 
         
         
